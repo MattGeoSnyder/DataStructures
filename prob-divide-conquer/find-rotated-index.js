d21a0f6ac74ea1e99588a084adf69537dd35a1ca
mattgeosnyder <mattgeosnyder@gmail.com>
2023-05-19 16:16:30 -0400

    Change rotated index. Think I've got infinite loop now. Great.

--- prob-divide-conquer/find-rotated-index.js
+++ prob-divide-conquer/find-rotated-index.js
@@ -2,29 +2,36 @@ function findRotatedIndex(arr, target) {
     let left = 0;
     let right = arr.length - 1;
 
-    while (left < right) {
+    while (left <= right) {
         let mid = Math.floor((left + right)/2);
+        let newLeft;
+        let newRight;
 
         if (arr[mid] === target) return mid;
 
         if (arr[mid] > target && arr[left] <= target) {
-            right = mid - 1;
+            newRight = mid - 1;
         }
 
         if (arr[mid] > target && arr[left] >= target) {
-            left = mid + 1;
+            newLeft = mid + 1;
         }
 
         if (arr[left] <= target && arr[mid] > target) {
-            right = mid - 1;
+            newRight = mid - 1;
         }
 
         if (arr[left] <= target && arr[mid] < target) {
-            left = mid + 1;
+            newLeft = mid + 1;
         }
+
+        left = newLeft || left;
+        right = newRight || right;
     }
 
     return -1;
 }
+
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)
 
 module.exports = findRotatedIndex
