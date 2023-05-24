ec4ce376b40aa526e4bbf27e4f1c74ed9a84ce69
mattgeosnyder <mattgeosnyder@gmail.com>
2023-05-24 15:50:58 -0400

    Finished pivot function. Working on remainder of quick sort.

--- dsa-sorting/insertion.js
+++ dsa-sorting/insertion.js
@@ -4,7 +4,7 @@ function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let curr = arr[i];
         let j = i - 1;
-        while ((j >= 0) && (current < arr[j])) {
+        while ((j >= 0) && (curr < arr[j])) {
             arr[j+1] = arr[j];
             j--;
         }
--- dsa-sorting/quick.js
+++ dsa-sorting/quick.js
@@ -3,12 +3,39 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start=0, end=arr.length){
+    let pivot = arr[start];
+    let idx = start
+    for (let i = start + 1; i < end; i++) {
+        if (arr[i] < pivot) {
+            arr[idx] = arr[i];
+            idx++
+            let j = i;
+            while (j > idx) {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[idx] = pivot;
+        }
+    }
+    return idx;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left=0, right=arr.length) {
+    if (right - left + 1 <= 1) {
+        return;
+    }
+    let idx = pivot(arr, left, right);
+    quickSort(arr, 0, idx - 1);
+    quickSort(arr, idx + 1, arr.length);
+    return arr;
+}
 
-module.exports = quickSort;
+var arr1 = [5, 4, 9, 10, 2, 20, 8, 7, 3];
+pivot(arr1);
+
+module.exports = { quickSort, pivot };
